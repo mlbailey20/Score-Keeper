6f08dba09c1eca603728d82ed93355aaf520696c
Matthew Bailey <incredibleducky@gmail.com>
2015-07-07 20:33:36 -0500

    Black Diamond Fail
    arrays aren't adding objects

--- Score Keeper/SKViewController.h
+++ Score Keeper/SKViewController.h
@@ -9,8 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface SKViewController : UIViewController
-
+@property (nonatomic, readwrite) NSMutableArray  *_scoreLabels;
+@property (nonatomic, readwrite) NSMutableArray *_scoreViews;
 @property (nonatomic) UIScrollView *scrollView;
-- (void) addScoreView:(int)n;
+@property (nonatomic) UIButton *addScore;
+@property (nonatomic) UIButton *removeScore;
+- (void) addScoreView:(id)sender;
+- (void) removeLastScore:(id)sender;
+- (void) updateButtonView;
 
 @end
--- Score Keeper/SKViewController.m
+++ Score Keeper/SKViewController.m
@@ -7,10 +7,10 @@
 //
 
 #import "SKViewController.h"
-float y = 0;
+int numViews = 0;
 @interface SKViewController ()
-@property (nonatomic, readwrite) NSMutableArray  *_scoreLabels;
-
+//@property (nonatomic, readwrite) NSMutableArray  *_scoreLabels;
+//@property (nonatomic, readwrite) NSMutableArray *_scoreViews;
 
 @end
 
@@ -23,14 +23,18 @@ float y = 0;
     
     self.scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), 1000)];
     
-    [self addScoreView:0];
+//    [self addScoreView:0];
+//    [self addScoreView:1];
+//    [self removeLastScore];
+    [self updateButtonView];
     [self.view addSubview:self.scrollView];
     
 
 }
 
-- (void) addScoreView:(int)n {
-    float y = ( 50 * n );
+- (void) addScoreView:(id)sender{
+    NSInteger n =  numViews;
+    float y = 50 + ( 50 * n );
     UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, y, CGRectGetWidth(self.view.frame), 50)];
     UITextField *name = [[UITextField alloc]initWithFrame:CGRectMake(0, y, 100, 50)];
         name.textColor = [UIColor cyanColor];
@@ -52,32 +56,71 @@ float y = 0;
     
     [button addTarget:self action:@selector(valueChanged:) forControlEvents:UIControlEventTouchUpInside];
     
-    
-    
-    
-    
-    
-   
-
-    
-    
     //add all to new view
     [view addSubview:name];
     [view addSubview:score];
     [view addSubview:button];
     
+    
     //add view to ScrollView
     
     [self.scrollView addSubview:view];
     
+    [__scoreViews addObject:<#(id)#>];
+      NSLog(@"%lu", (unsigned long)[__scoreViews count]);
+    numViews++;
+    //NSLog(@"%d",numViews);
     
 }
 
+-(void) removeLastScore:(id)sender{
+    NSLog(@"%lu", (unsigned long)[__scoreViews count]);
+    [__scoreViews[numViews-1] removeFromSuperview];
+    [__scoreLabels removeLastObject];
+    numViews--;
+    
+}
+
+- (void) updateButtonView{
+    
+    if(self.addScore)
+    {
+        
+    }
+    else
+    {
+        _addScore = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+       UILabel *addScoreLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+        addScoreLabel.text = @"ADD";
+        _addScore.backgroundColor = [UIColor greenColor];
+        [_addScore addSubview:addScoreLabel];
+        [self.scrollView addSubview:_addScore];
+        
+        
+    }
+    
+    
+    if(self.removeScore)
+    {
+    
+    }
+    else
+    {
+        _removeScore = [[UIButton alloc] initWithFrame:CGRectMake(50, 0, 50, 50)];
+        UILabel *removeScoreLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+        removeScoreLabel.text = @"REM";
+        _removeScore.backgroundColor = [UIColor redColor];
+        [_removeScore addSubview:removeScoreLabel];
+        [self.scrollView addSubview:_removeScore];
+    }
+    [_addScore addTarget:self action:@selector(addScoreView:) forControlEvents:UIControlEventTouchUpInside];
+    [_removeScore addTarget:self action:@selector(removeLastScore:) forControlEvents:UIControlEventTouchUpInside];
+}
 
 - (void) valueChanged: (id) sender {
     UIStepper *new = [[UIStepper alloc] init];
     new = sender;
-    NSInteger index = new.tag;
+    
     NSLog(@"%f", new.value);
 
     double value = [new value];
